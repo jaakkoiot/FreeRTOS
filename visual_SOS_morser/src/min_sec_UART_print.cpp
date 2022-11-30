9ad9bad75cdb66de56378c342fc231983ce647e2
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-12-01 00:37:35 +0200

    init for SOS morser

---
+++ visual_SOS_morser/src/min_sec_UART_print.cpp
@@ -0,0 +1,164 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "heap_lock_monitor.h"
+
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* Sets up system hardware */
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Initial LED0 state is off */
+	Board_LED_Set(0, false);
+}
+
+/* LED1 toggle thread */
+static void vLEDTask1(void *pvParameters) {
+	bool LedState = false;
+
+	while (1) {
+		Board_LED_Set(0, LedState);
+		LedState = (bool) !LedState;
+
+		/* About a 3Hz on/off toggle rate */
+		vTaskDelay(configTICK_RATE_HZ / 2);
+	}
+}
+
+/* LED2 toggle thread */
+static void vLEDTask2(void *pvParameters) {
+	bool LedState = false;
+
+	while (1) {
+		Board_LED_Set(1, LedState);
+		LedState = (bool) !LedState;
+
+		/* About a 7Hz on/off toggle rate */
+		vTaskDelay(configTICK_RATE_HZ / 3);
+	}
+}
+
+/*LED 3 toggle thread*/
+static void vLEDTask3(void *pvParameters) {
+	bool LedState = true;
+
+	while(1){
+		Board_LED_Set(2, LedState);
+		LedState = (bool) !LedState;
+
+		vTaskDelay(configTICK_RATE_HZ / 5);
+	}
+}
+
+/* UART (or output) thread */
+static void vUARTTask(void *pvParameters) {
+	int sec = 0;
+	int min = 0;
+
+	while (1) {
+		DEBUGOUT("Time: %02d:%02d \r\n", min, sec);
+		sec++;
+
+		if(sec == 60){
+			min++;
+			if(min == 60){
+				min = 0;
+			}
+			sec = 0;
+			DEBUGOUT("-- -- --\n");
+		}
+
+		/* About a 1s delay here */
+		vTaskDelay(configTICK_RATE_HZ);
+	}
+}
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/* the following is required if runtime statistics are to be collected */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
+
+/**
+ * @brief	main routine for FreeRTOS blinky example
+ * @return	Nothing, function should not exit
+ */
+int main(void)
+{
+	prvSetupHardware();
+
+	heap_monitor_setup();
+
+	/* LED1 toggle thread */
+	xTaskCreate(vLEDTask1, "vTaskLed1",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/* LED2 toggle thread */
+	xTaskCreate(vLEDTask2, "vTaskLed2",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/*LED 3 toggle thread*/
+	xTaskCreate(vLEDTask3, "vTaskLed3",
+			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+	/* UART output thread, simply counts seconds */
+	xTaskCreate(vUARTTask, "vTaskUart",
+				configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
+
