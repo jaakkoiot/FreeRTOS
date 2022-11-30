9587e4d01fde62b85e676d8b6b2dfa7b1aa5d802
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-11-30 23:01:35 +0200

    libraries set to newlib (nohost) - first succesful build

--- 01_pt1_min_sec_UART/src/01_pt1_min_sec_UART.cpp
+++ 01_pt1_min_sec_UART/src/01_pt1_min_sec_UART.cpp
@@ -22,30 +22,116 @@
 
 // TODO: insert other definitions and declarations here
 
-int main(void) {
+#include "FreeRTOS.h"
+#include "task.h"
+#include "heap_lock_monitor.h"
 
-#if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
 
-    // TODO: insert code here
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
-    }
-    return 0 ;
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* Sets up system hardware */
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Initial LED0 state is off */
+	Board_LED_Set(0, false);
 }
+
+/* LED1 toggle thread */
+static void vLEDTask1(void *pvParameters) {
+	bool LedState = false;
+
+	while (1) {
+		Board_LED_Set(0, LedState);
+		LedState = (bool) !LedState;
+
+		/* About a 3Hz on/off toggle rate */
+		vTaskDelay(configTICK_RATE_HZ / 6);
+	}
+}
+
+/* LED2 toggle thread */
+static void vLEDTask2(void *pvParameters) {
+	bool LedState = false;
+
+	while (1) {
+		Board_LED_Set(1, LedState);
+		LedState = (bool) !LedState;
+
+		/* About a 7Hz on/off toggle rate */
+		vTaskDelay(configTICK_RATE_HZ / 14);
+	}
+}
+
+/* UART (or output) thread */
+static void vUARTTask(void *pvParameters) {
+	int tickCnt = 0;
+
+	while (1) {
+		DEBUGOUT("Tick: %d \r\n", tickCnt);
+		tickCnt++;
+
+		/* About a 1s delay here */
+		vTaskDelay(configTICK_RATE_HZ);
+	}
+}
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/* the following is required if runtime statistics are to be collected */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
+
+/**
+ * @brief	main routine for FreeRTOS blinky example
+ * @return	Nothing, function should not exit
+ */
+int main(void)
+{
+	prvSetupHardware();
+
+	heap_monitor_setup();
+
+	/* LED1 toggle thread */
+	xTaskCreate(vLEDTask1, "vTaskLed1",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/* LED2 toggle thread */
+	xTaskCreate(vLEDTask2, "vTaskLed2",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/* UART output thread, simply counts seconds */
+	xTaskCreate(vUARTTask, "vTaskUart",
+				configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
+				(TaskHandle_t *) NULL);
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
+
